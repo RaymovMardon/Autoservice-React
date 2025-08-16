b529904d479515234aaf70373152d5c619121738
RaymovMardon <googlaccnew12@gmail.com>
2025-08-17 04:17:35 +0500

    edit btn, margin, responsive,add countUp 17.08.25 4:17

--- src/Components/SectionCount/SectionCount.jsx
+++ src/Components/SectionCount/SectionCount.jsx
@@ -1,23 +1,65 @@
-import { React, useEffect, useRef, useState } from "react";
+import { React } from "react";
 import CountUp from "react-countup";
 import { useInView } from "react-intersection-observer";
+import video from "../../../public/video2.mp4";
+import "./SectionCount.css";
 const SectionCount = () => {
   const { ref, inView } = useInView({
-    threshold: 1, // 50% элемента должно быть видно
+    threshold: 0, // 50% элемента должно быть видно
     triggerOnce: false, // только один раз запускать
   });
 
   return (
     <>
-      <section ref={ref}>
-        
-        {inView ? (
-          <div>
-            <CountUp start={0} end={100} delay={0.5} duration={5.75} />
-          </div>
-        ) : (
-          ""
-        )}
+      <section ref={ref} id="section_count">
+        <div className="section_count_relative">
+          {inView ? (
+            <div className="section_absolute">
+              <h2>Нам доверяют</h2>
+              <div className="section_count_wrp">
+                <div>
+                <i class='bx  bx-user-check'  ></i> 
+                  <h6>Мастеров:</h6>
+                  <span>+
+                    <CountUp start={0} end={35} delay={0.5} duration={5.75} />
+                  </span>
+                </div>
+                <div>
+                  {" "}
+                  <i class="bx  bx-package"></i>
+                  <h6>Автозапчастей:</h6>
+                  <span>
+                    <CountUp
+                      start={5000}
+                      end={10000}
+                      delay={0.5}
+                      duration={4.75}
+                    />
+                  </span>
+                </div>
+                <div>
+                  {" "}
+                  <i class='bx  bx-like'  ></i> 
+                  <h6>Довольных клиентов:</h6>
+                  <span>+
+                    <CountUp
+                      start={700}
+                      end={1000}
+                      delay={0.5}
+                      duration={2.75}
+                    />
+                  </span>
+                </div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+        <video autoPlay loop muted playsInline>
+          <source src={video} type="video/mp4" />
+          Ваш браузер не поддерживает видео.
+        </video>
       </section>
     </>
   );
