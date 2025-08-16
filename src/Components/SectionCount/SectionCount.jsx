96634d919c6b997c510b3ccfb23fdb890e1676e2
RaymovMardon <googlaccnew12@gmail.com>
2025-08-16 23:51:56 +0500

    edit btn, margin, responsive,add countUp 16.08.25 23:51

--- src/Components/SectionCount/SectionCount.jsx
+++ src/Components/SectionCount/SectionCount.jsx
@@ -1,54 +1,23 @@
 import { React, useEffect, useRef, useState } from "react";
 import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
 const SectionCount = () => {
-  const sectionRef = useRef(null);
-  const [inView, setInView] = useState(false);
-  /*useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setInView(true);
-          observer.disconnect(); // Запускаем один раз
-        }
-      },
-      {
-        threshold: 0.5, // Сколько % секции должно быть видно
-      }
-    );
+  const { ref, inView } = useInView({
+    threshold: 1, // 50% элемента должно быть видно
+    triggerOnce: false, // только один раз запускать
+  });
 
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current);
-    }
-
-    return () => observer.disconnect();
-  }, []);*/
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setInView(true); // 4️⃣ Меняем состояние, когда элемент в зоне видимости
-          observer.disconnect(); // (по желанию) отключаем наблюдение
-        }
-      },
-      {
-        threshold: 0.5, // 3️⃣ Условие: элемент должен быть хотя бы на 50% виден
-      }
-    );
-    console.log(inView);
-
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current); // 2️⃣ Передаём DOM-элемент в observer
-    }
-
-    return () => observer.disconnect(); // 🧹 Очистка
-  }, []);
   return (
     <>
-      <section ref={sectionRef}>
-        <div>
-          {console.log(inView, sectionRef)}
-          <CountUp start={0} end={100} delay={5} duration={2.75} />
-        </div>
+      <section ref={ref}>
+        
+        {inView ? (
+          <div>
+            <CountUp start={0} end={100} delay={0.5} duration={5.75} />
+          </div>
+        ) : (
+          ""
+        )}
       </section>
     </>
   );
