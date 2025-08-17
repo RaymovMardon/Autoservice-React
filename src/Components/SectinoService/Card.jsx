06763cce73b6f522f716e65caa299acf5303e83a
RaymovMardon <googlaccnew12@gmail.com>
2025-08-18 01:23:17 +0500

    edit btn, margin, responsive,add countUp 18.08.25 1:23

--- src/Components/SectinoService/Card.jsx
+++ src/Components/SectinoService/Card.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Card = ({ img, p }) => {
- 
+const Card = ({ img, p, info }) => {
+  const [flipped, setFlipped] = useState(false);
 
   return (
-    <>
-     
-        <div className="service-card">
-          <i class={img}></i>
+    <div className="service-card" onClick={() => setFlipped(!flipped)}>
+      <div className={`card-inner ${flipped ? "flipped" : ""}`}>
+        {/* Front */}
+        <div className="card-front">
+          <i className={img}></i>
           <p>{p}</p>
         </div>
-      
-    </>
+
+        {/* Back */}
+        <div className="card-back">
+          <p>{info}</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/Components/SectinoService/SectionService.jsx
+++ src/Components/SectinoService/SectionService.jsx
@@ -6,22 +6,27 @@ const SectionService = () => {
     {
       img: "bx bxs-car-battery",
       p: "Авто-Электрик",
+      info: "Tel: +998 (94) 618 45 72",
     },
-    { img: "bx bxs-wrench", p: "Авто-Механик" },
-    { img: "bx bxs-car-mechanic", p: "Ремонт Авто на Выезд" },
-    { img: "bx bxs-wrench", p: "Авто-Вулканизация" },
-    { img: "bx bxs-lock-open-alt", p: "Вскрытия Автомобиля" },
-    { img: "bx bx-time-five", p: "Выезд Автомастер Круглосуточный" },
-    { img: "bx bxs-car-battery", p: "Выезд Автоэлектрик Круглосуточный" },
-    { img: "bx bxs-tachometer", p: "Замена Масла" },
-    { img: "bx bxs-car-crash", p: "Замена Шин" },
-    { img: "bx bxs-cog", p: "Обслуживание Двигателя" },
-    { img: "bx bx-pulse", p: "Диагностика" },
-    { img: "bx bxs-wrench", p: "Сварочная работа" },
-    { img: "bx bxs-factory", p: "Токарная услуга" },
-    { img: "bx bxs-car", p: "Тонировка" },
-    { img: "bx bxs-cog", p: "Моторист" },
-    { img: "bx bxs-car-crash", p: "Кастаправ" },
+    {
+      img: "bx bxs-wrench",
+      p: "Авто-Механик",
+      info: "Tel: +998 (97) 717 11 18",
+    },
+    { img: "bx bxs-car-mechanic", p: "Ремонт Авто на Выезд",info: "Tel: +998 (93) 543 42 10", },
+    { img: "bx bxs-wrench", p: "Авто-Вулканизация",info: "Tel: +998 (93) 566 35 72", },
+    { img: "bx bxs-lock-open-alt", p: "Вскрытия Автомобиля",info: "Tel: +998 (98) 818 12 19", },
+    { img: "bx bx-time-five", p: "Выезд Автомастер Круглосуточный",info: "Tel: +998 (90) 466 55 18", },
+    { img: "bx bxs-car-battery", p: "Выезд Автоэлектрик Круглосуточный",info: "Tel: +998 (95) 715 84 22", },
+    { img: "bx bxs-tachometer", p: "Замена Масла",info: "Tel: +998 (93) 407 00 56", },
+    { img: "bx bxs-car-crash", p: "Замена Шин",info: "Tel: +998 (99) 756 22 98", },
+    { img: "bx bxs-cog", p: "Обслуживание Двигателя",info: "Tel: +998 (91) 911 12 88", },
+    { img: "bx bx-pulse", p: "Диагностика",info: "Tel: +998 (97) 700 10 16", },
+    { img: "bx bxs-wrench", p: "Сварочная работа",info: "Tel: +998 (94) 569 00 10", },
+    { img: "bx bxs-factory", p: "Токарная услуга",info: "Tel: +998 (97) 745 98 00", },
+    { img: "bx bxs-car", p: "Тонировка",info: "Tel: +998 (95) 727 56 48", },
+    { img: "bx bxs-cog", p: "Моторист",info: "Tel: +998 (97) 738 72 75", },
+    { img: "bx bxs-car-crash", p: "Кастаправ",info: "Tel: +998 (90) 455 22 87", },
   ];
   return (
     <>
@@ -30,10 +35,10 @@ const SectionService = () => {
           <h2>Наши Услуги</h2>
           <h3>Ознакомьтесь С Нашими Услугами</h3>
           <div className="service-cards">
-          {card.map((todo, index) => (
-            <Card key={index} img={todo.img} p={todo.p} />
-          ))}
-        </div>
+            {card.map((todo, index) => (
+              <Card key={index} img={todo.img} p={todo.p} info={todo.info} />
+            ))}
+          </div>
         </div>
       </section>
     </>
